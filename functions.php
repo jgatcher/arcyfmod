718b870703af003572eb3146f150982c00b57503
joseph <jgatcher@gmail.com>
2016-06-05 07:13:47 -0500

    added cover.css styles for camp landing page

--- functions.php
+++ functions.php
@@ -1,9 +1,8 @@
 <?php
 include_once(ABSPATH .'wp-content/themes/post-types.php');
 function b2w_theme_styles(){
-
+   wp_enqueue_style('cover_css',get_template_directory_uri() . '/assets/css/cover.css');
 	wp_enqueue_style('bootstrap_css',get_template_directory_uri() . '/bootstrap3/css/bootstrap.min.css');
-	wp_enqueue_style('ct_paper_css',get_template_directory_uri() . '/assets/css/ct-paper.css');
 	wp_enqueue_style('ct_paper_css',get_template_directory_uri() . '/assets/css/ct-paper.css');
 	wp_enqueue_style('demo_css',get_template_directory_uri() . '/assets/css/demo.css');
 	wp_enqueue_style('style_css',get_template_directory_uri() . '/style.css');
